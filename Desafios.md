b80514e7c90c43fef9d4d1a426df41fe9d119c20
joaoapel <joaoapel@gmail.com>
2024-07-20 16:12:58 -0300

    Primeiro commit

---
+++ Desafios.md
@@ -0,0 +1,26 @@
+# Aprendizado
+
+- O iex é iex.bat no Windows ATENÇÃO
+- CTRL + C para sair do IEx
+- https://www.youtube.com/watch?v=A_43Iy9nCCo
+- mix new todos : para criar novo projeto
+- Roda o projeto, aí voce tem acesso as funções: iex.bat -S mix 
+- Depois de rodar o comando acima: colocar Iex:DesafioCli.main("oi")
+- Digitar 'recompile' ao atualizar o código para atualizar os símbolos no IEx
+- clear no IEx funciona
+- https://hexdocs.pm/elixir/1.17.0/IO.html
+- _variavel para variáveis não utilizadas
+- Destruturação é igual unpacking no Javascript {a, b} = {coisa, coisa1}
+- Módulo Enum -> frequencies
+-   # for i <- [1,2,3], do: i*i
+
+  # for i <- 1..55, do: i*i
+
+  # import Integer
+  # for i <- 1..30, Integer.is_even(i), do: i*i
+
+  # for x <- [1,2,3], y <- [2,3,4], do: x * y
+
+  -https://stackoverflow.com/questions/56210712/how-to-cleanly-take-multiline-input-from-the-terminal-using-elixir
+
+  - escript .\desafio_cli
--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -7,11 +7,74 @@ defmodule DesafioCli do
   A função main recebe os argumentos passados na linha de
   comando como lista de strings e executa a CLI.
   """
+
   def main(_args) do
-    IO.puts(phrase())
+   pegar_nomes()
+   |> Enum.frequencies()
+   |> Enum.map(fn {k, v} -> {k, to_roman(v)} end)
+   |> Enum.map(fn ({key, value}) -> IO.puts [key <> " " <> value] end)
   end
 
-  def phrase() do
-    "Hello, world!"
+  def to_roman(number, roman_accumulator) when number >= 1000 do
+    to_roman(number - 1000, roman_accumulator <> "M")
   end
+
+  def to_roman(number, roman_accumulator) when number >= 900 do
+    to_roman(number - 900, roman_accumulator <> "CM")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 500 do
+    to_roman(number - 500, roman_accumulator <> "D")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 400 do
+    to_roman(number - 400, roman_accumulator <> "CD")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 100 do
+    to_roman(number - 100, roman_accumulator <> "C")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 90 do
+    to_roman(number - 90, roman_accumulator <> "XC")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 50 do
+    to_roman(number - 50, roman_accumulator <> "L")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 40 do
+    to_roman(number - 40, roman_accumulator <> "XL")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 10 do
+    to_roman(number - 10, roman_accumulator <> "X")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 9 do
+    to_roman(number - 9, roman_accumulator <> "IX")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 5 do
+    to_roman(number - 5, roman_accumulator <> "V")
+  end
+
+  def to_roman(number, roman_accumulator) when number == 4 do
+    roman_accumulator <> "IV"
+  end
+
+  def to_roman(number, roman_accumulator) when number <= 3 do
+    roman_accumulator <> String.duplicate("I", number)
+  end
+
+  def to_roman(number) do
+    to_roman(number, "")
+  end
+
+  def pegar_nomes do
+    IO.stream(:stdio, :line)
+    |> Enum.take_while(&(&1 != "\n"))
+    |> Enum.map(&String.replace(&1, "\n", ""))
+  end
+
 end
